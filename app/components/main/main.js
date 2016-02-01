7f8b6d360f4969d4224dffd8198dd44e9f09c3f6
Rachel bruk <rachel@linnovate.net>
2016-02-01 11:50:25 +0200

    change matrix to react

--- app/components/main/main.js
+++ app/components/main/main.js
@@ -1,21 +1,406 @@
 "use strict";
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import DataService from '../../services/DataService.js';
 
+var rows_global = 3, cols_global = 5;
+var romMarginRight = 0, rowMarginBottom = 0;
+var rowWidth = 100/cols_global;
+var rowsWrapRight = romMarginRight/2;
+var matrixSlides = '';
+var index = 0;
 
-class ArticleCaptcha extends HTMLElement {
-
-
-    createdCallback() {
-        this.innerHTML = this.template();
+class MatrixCtrl extends React.Component {
+    render(){
+        console.log(this.props.data,'data');
+        var src = this.props.data.public_properties.src_small_img;
+        return <div  className = "row" search = {this.props.data.title}>
+                    <div className="row-in">
+                        <span>{this.props.data.title}</span>
+                        <img src = {src} />
+                    </div>
+                </div>;
     }
+}
 
-    template() {
-        return `<img src='app/assets/images/img.jpg'/>
-                <div class='captcha'>
-                    <h3>Title</h3>
-                    <div>some text some textsome textsome textsome textsome textsome text</div>
-                </div>`;
+export default MatrixCtrl;
+
+class MatrixCol extends React.Component {
+    constructor(props) {
+        super(props);
+        this.MatrixData = props.data;
+        
+    }
+    render(){
+        var items = [];
+            for(var i = 0; i < cols_global; i++) {
+                items.push(<MatrixCtrl key={i} data={this.MatrixData[index]} />);
+                console.log(index,'index');
+                index++;
+            }
+            return <div>{items}</div>;
+    }
+}
+
+export default MatrixCol;
+
+class MatrixRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.MatrixData = props.data;
+        
+    }
+    render(){
+        var items = [];
+            for(var j = 0; j < rows_global; j++) {
+                items.push(<div key={j} className="rows"><MatrixCol data={this.MatrixData} /></div>);
+            }
+            return <div>{items}</div>;
+    }
+}
+
+export default MatrixRow;
+
+class MainComponent extends React.Component { 
+    constructor(props) {
+        super(props);
+this.MatrixData = [
+    {
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "llllllllllll",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "iiiiiiiiii",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "yyyyyyy",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "rrrrrr",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "tttttt",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "llllllllllll",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "iiiiiiiiii",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "yyyyyyy",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "rrrrrr",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "tttttt",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "llllllllllll",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "iiiiiiiiii",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "yyyyyyy",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "rrrrrr",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "tttttt",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "llllllllllll",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "iiiiiiiiii",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "yyyyyyy",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "rrrrrr",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    },{
+        "tags": "[ ]",
+        "date_updated": "2016-01-12T19:20:50.664150",
+        "title": "tttttt",
+        "public_properties": {
+            "small_sub_title": "",
+            "second_button_href": "",
+            "button_caption": "",
+            "title": "Bora",
+            "sub_title": "werlkgjerl;gj;lerjg;ler",
+            "src_big_img": "//media.wixapps.net/wix-a091529b-0151-4768-a83e-4cb899c90de2/images/b9a5e5030e9f4055a51503f737134878/v1/fill/w_1920,h_816,al_c,q_75/file.jpg",
+            "src_small_img": "http://static.wixstatic.com/media/a09152_2d1b97f81ced43ec857171dfca15eba3.jpg//v1/fill/w_321,h_200/file.jpg",
+            "first_button_href": ""
+        }
+
+    }
+];   
+}
+render() {
+    return <div>
+            <div className="matrix-container">
+                <div className="search-wrap">
+                    <div className="search">
+                        <input type="search" name="search"  />
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </div>
+            </div>
+            <div className="matrix-gallery">
+                <div className="matrix-gallery-inner">
+                <MatrixRow data={this.MatrixData}/>
+                </div>
+            </div>
+        </div>;
+    }
+}
+
+export default MainComponent;
+
+
+
+
+class MainComponent1 extends React.Component {
+    render() {
+        
+        return (
+            
+            <MainComponent />
+    );
     }
 
 }
 
-document.registerElement('article-captcha', ArticleCaptcha);
+export default MainComponent1;
---
+++ appSettings/services/youtube/youtube.js
@@ -0,0 +1,137 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+
+
+const OAUTH2_CLIENT_ID = '120623149106-e1vc0be0ho8htjfc4t445lkeme7cl15m.apps.googleusercontent.com';
+const OAUTH2_SCOPES = [
+    'https://www.googleapis.com/auth/youtube.readonly'
+];
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+console.log("tag",tag.src);
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+window.onJSClientLoad = function() {
+    console.log("onJSClientLoad");
+    gapi.auth.init(function() {
+        window.setTimeout(checkAuth, 1);
+    });
+};
+function checkAuth() {
+    console.log("checkAuth");
+    gapi.auth.authorize({
+        client_id: OAUTH2_CLIENT_ID,
+        scope: OAUTH2_SCOPES,
+        immediate: false
+    }, handleAuthResult);
+}
+function handleAuthResult(authResult) {
+    console.log("authResult",authResult);
+    if (authResult) {
+
+        // Authorization was successful. Hide authorization prompts and show
+        // content that should be visible after authorization succeeds.
+        $('.pre-auth').hide();
+        $('.post-auth').show();
+
+        loadAPIClientInterfaces();
+    } else {
+        // Authorization was unsuccessful. Show content related to prompting for
+        // authorization and hide content that should be visible if authorization
+        // succeeds.
+        $('.post-auth').hide();
+        $('.pre-auth').show();
+
+        // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
+        // client flow. The current function is called when that flow completes.
+        $('#login-link').click(function() {
+            console.log("fdfdg");
+            gapi.auth.authorize({
+                client_id: OAUTH2_CLIENT_ID,
+                scope: OAUTH2_SCOPES,
+                immediate: false
+            }, handleAuthResult);
+        });
+    }
+}
+function loadAPIClientInterfaces() {
+
+    gapi.client.load('youtube', 'v3', function() {
+        handleAPILoaded();
+    });
+}
+function handleAPILoaded() {
+    $('#search-button').attr('disabled', false);
+}
+class SearchYoutube extends Component{
+    constructor() {
+        super();
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+    handleSearch(event){
+        event.preventDefault();
+        var component=this;
+        var playlistId, nextPageToken,prevPageToken;
+        var searchString = this.input.value;
+        //var request = gapi.client.youtube.channels.list({
+        //    mine: true,
+        //    part: 'contentDetails'
+        //});
+        //request.execute(function(response) {
+        //    playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+        //    console.log("playlistId",playlistId);
+        //    requestVideoPlaylist('PL9XmHn-vbHMBjdh094kfJ9PAnBgyEg6ty');
+        //
+        //});
+        requestVideoPlaylist(searchString);
+        function requestVideoPlaylist(playlistId, pageToken) {
+            $('#video-container').html('');
+            var requestOptions = {
+                playlistId: playlistId,
+                part: 'snippet',
+                maxResults: 10
+            };
+            if (pageToken) {
+                requestOptions.pageToken = pageToken;
+            }
+            var request = gapi.client.youtube.playlistItems.list(requestOptions);
+            request.execute(function(response) {
+                //nextPageToken = response.result.nextPageToken;
+                //var nextVis = nextPageToken ? 'visible' : 'hidden';
+                //$('#next-button').css('visibility', nextVis);
+                //prevPageToken = response.result.prevPageToken
+               // var prevVis = prevPageToken ? 'visible' : 'hidden';
+                //$('#prev-button').css('visibility', prevVis);
+                var playlistItems = response.result.items;
+                console.log("playlistItems",playlistItems);
+                if (playlistItems) {
+                    $.each(playlistItems, function(index, item) {
+                        displayResult(item.snippet);
+                    });
+                } else {
+                    $('#video-container').html('Sorry you have no uploaded videos');
+                }
+            });
+        }
+        function displayResult(videoSnippet) {
+            var title = videoSnippet.title;
+            var videoId = videoSnippet.resourceId.videoId;
+            $('#video-container').append('<p>' + title + ' - ' + videoId + '</p>');
+        }
+  }
+    render() {
+    return (
+        <div className="commentBox1">
+            <div id="buttons">
+                <input id="query" type="text" ref={node => {this.input=node}} /><button id="search-button" onClick={this.handleSearch}>Search</button>
+            </div>
+            <div id="video-container"></div>
+            <div id="search-container"></div>
+        </div>
+
+    );
+}
+
+}
+
+export default SearchYoutube;

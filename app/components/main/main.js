b52985a2d37700564bd1fb90cd4af6c510fe4a23
Rachel bruk <rachel@linnovate.net>
2016-02-03 13:19:57 +0200

    change the matrix build by css

--- app/components/main/main.js
+++ app/components/main/main.js
@@ -3,24 +3,12 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import DataService from '../../services/DataService.json';
 
-var rows_global = 3, cols_global = 5;
+var rows_global = 2, cols_global = 5;
 var romMarginRight = 0, rowMarginBottom = 0;
 var rowWidth = 100/cols_global;
 var rowsWrapRight = romMarginRight/2;
 var matrixSlides = '';
 var index = 0;
-
-Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, function (style) {
-    var matrixMarginBetweenColumns, matrixMarginBetweenRows, index;
-    matrixMarginBetweenColumns = document.querySelectorAll(".rows .row");
-    for (index = 0; index < matrixMarginBetweenColumns.length; index++) {
-        matrixMarginBetweenColumns[index].style.marginRight = style.numbers.HorizontalMarginBetweenColumns + 'px';
-    }
-    matrixMarginBetweenRows = document.querySelectorAll(".rows .row");
-    for (index = 0; index < matrixMarginBetweenRows.length; index++) {
-        matrixMarginBetweenRows[index].style.marginBottom = style.numbers.VerticalMarginBetweenRows + 'px';
-    }
-});
 
 class Search extends React.Component {
     render(){
@@ -29,100 +17,137 @@ class Search extends React.Component {
 }
 ReactDOM.render(<Search />,document.getElementsByTagName('head')[0]);
 
-class MatrixCtrl extends React.Component {
+// class MatrixCtrl extends React.Component {
 
-    componentDidMount() {
-        var elem = document.querySelectorAll('.row');
-        for(var i = 0; i < elem.length; i++) {
-            elem[i].style.width = rowWidth + '%';
-        }
-        var rowIn = document.querySelectorAll('.matrix-gallery-inner .row-in');
-        // var img = document.querySelectorAll('.matrix-gallery-inner .row-in img');
-        for(var i = 0; i < rowIn.length; i++) {
-            rowIn[i].style.marginRight = romMarginRight;
-            rowIn[i].style.marginBottom = rowMarginBottom;
-        //     img[i].style.opacity = 0.1;
-        }
-        // setTimeout (function(){
-        //     var maxHeight = 0;
-        //     rowIn[i].style.height = 'auto';
-        //     rowIn[i].each(function() {
-        //     var thisHeight = $(this).height();
-        //         if (thisHeight > maxHeight) {
-        //             maxHeight = thisHeight;
-        //         }
-        //     });
-        //     rowIn.height(function(i,val){
-        //         if (val < maxHeight) {
-        //             val=maxHeight;
-        //         }
-        //         return val;
-        //     });
-        //     // rowIn.find('img').css({opacity: 1});
-        //     // rowIn.find('span').css({lineHeight: maxHeight + 'px'});
-        // }, 500); 
+//     componentDidMount() {
+//         var elem = document.querySelectorAll('.row');
+//         for(var i = 0; i < elem.length; i++) {
+//             elem[i].style.width = rowWidth + '%';
+//         }
+//         var rowIn = document.querySelectorAll('.matrix-gallery-inner .row-in');
+//         // var img = document.querySelectorAll('.matrix-gallery-inner .row-in img');
+//         for(var i = 0; i < rowIn.length; i++) {
+//             rowIn[i].style.marginRight = romMarginRight;
+//             rowIn[i].style.marginBottom = rowMarginBottom;
+//         //     img[i].style.opacity = 0.1;
+//         }
+//         // setTimeout (function(){
+//         //     var maxHeight = 0;
+//         //     rowIn[i].style.height = 'auto';
+//         //     rowIn[i].each(function() {
+//         //     var thisHeight = $(this).height();
+//         //         if (thisHeight > maxHeight) {
+//         //             maxHeight = thisHeight;
+//         //         }
+//         //     });
+//         //     rowIn.height(function(i,val){
+//         //         if (val < maxHeight) {
+//         //             val=maxHeight;
+//         //         }
+//         //         return val;
+//         //     });
+//         //     // rowIn.find('img').css({opacity: 1});
+//         //     // rowIn.find('span').css({lineHeight: maxHeight + 'px'});
+//         // }, 500); 
+//     }
+//     render(){
+//         console.log(this.props.data.public_properties,'data');
+//         var src = this.props.data.public_properties.src_small_img;
+//         return <div data-search ={this.props.data.title}  className = "row" >
+//                     <div className="row-in">
+//                         <span>{this.props.data.title}</span>
+//                         <img src = {src} />
+//                     </div>
+//                 </div>;
+//     }
+// }
+
+
+
+class MatrixItem extends React.Component {
+
+    render(){
+        return   <li className="row" data-search ={this.props.title} >
+                    <span>{this.props.title}</span>
+                    <img src = {this.props.src} />
+                </li>;
+    }
+}
+
+class MatrixList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.MatrixData = props.data;
     }
     render(){
-        var src = this.props.data.public_properties.src_small_img;
-        return <div data-search ={this.props.data.title}  className = "row" >
-                    <div className="row-in">
-                        <span>{this.props.data.title}</span>
-                        <img src = {src} />
+            return <ul className="list rows">
+                {this.MatrixData.map((item, i)=>
+                  <MatrixItem key={i} title={item.title} src={item.public_properties.src_small_img} />
+                )}
+            </ul>;
+    }
+}
+
+class MainComponent extends React.Component {
+    constructor(props) { 
+        super(props);
+        Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, function (style) {
+            console.log(style,'style'); 
+            // if(style.fonts.Skin.index === 1) {
+                cols_global = style.numbers.RowsColumns ? style.numbers.RowsColumns: 5;
+                rows_global = style.numbers.RowsNumber ? style.numbers.RowsNumber : 1;
+                // ReactDOM.render(<MainComponent />,document.getElementById('root'));
+
+                var matrixMarginBetweenColumns, matrixMarginBetweenRows, index;
+                matrixMarginBetweenColumns = document.querySelectorAll(".rows .row");
+                for (index = 0; index < matrixMarginBetweenColumns.length; index++) {
+                    matrixMarginBetweenColumns[index].style.marginRight = style.numbers.HorizontalMarginBetweenColumns + 'px';
+                }
+                matrixMarginBetweenRows = document.querySelectorAll(".rows .row");
+                for (index = 0; index < matrixMarginBetweenRows.length; index++) {
+                    matrixMarginBetweenRows[index].style.marginBottom = style.numbers.VerticalMarginBetweenRows + 'px';
+                }
+            // }
+        });  
+    this.MatrixData = MatrixData;
+    this.cols = cols_global;
+    this.MatrixData = this.MatrixData.splice(0, rows_global * cols_global); 
+}
+
+onChange(e) {
+    var val = e.target.value;
+    var searchTag= document.getElementById('search');
+    searchTag.innerHTML = val === '' ? '' : '.row:not([data-search*="'+ val +'"]){order:0; opacity: .3;}';
+}
+render() {
+    return <div>
+            <div className="matrix-container">
+                <div className="search-wrap">
+                    <div className="search">
+                        <input type="search" name="search" onChange={this.onChange.bind(this)}/>
+                        <input type="submit" value="Submit"/>
                     </div>
-                </div>;
+                </div>
+            </div>
+            <div className="matrix-gallery">
+                <div className="matrix-gallery-inner">
+                <MatrixList data={this.MatrixData}/>
+                </div>
+            </div>
+        </div>;
     }
 }
 
-    
-export default MatrixCtrl;
+export default MainComponent;
 
-class MatrixCol extends React.Component {
-    constructor(props) {
-        super(props);
-        this.MatrixData = props.data;
-        
-    }
-    render(){
-        var items = [];
-            for(var i = 0; i < cols_global; i++) {
-                items.push(<MatrixCtrl key={i} data={this.MatrixData[index]} />);
-                index++;
-            }
-            return <div className="Row">{items}</div>;
-    }
-}
 
-export default MatrixCol;
 
-class MatrixRow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.MatrixData = props.data;
-        
-    }
-    render(){
-        var items = [];
-            for(var j = 0; j < rows_global; j++) {
-                items.push(<div key={j} className="rows"><MatrixCol data={this.MatrixData} /></div>);
-            }
-            return <div>{items}</div>;
-    }
-}
 
-export default MatrixRow;
 
-class MainComponent extends React.Component { 
-    constructor(props) {
-        super(props);
-        // document.getElementsByClassName('.matrix-gallery .rows-wrap').style.left = rowsWrapRight;
-        // document.getElementsByClassName('.matrix-gallery .rows-wrap').style.matrginBottom = rowMarginBottom;
-    
 
-        // var rowIn = $('.matrix-gallery-inner .row-in');
-        // rowIn.css({marginRight: romMarginRight, marginBottom: rowMarginBottom});
-        // rowIn.find('img').css({opacity: 0.1});
+////////////////////////////////////////////////////////
 
-this.MatrixData = [
+var MatrixData = [
     {
         "tags": "[ ]",
         "date_updated": "2016-01-12T19:20:50.664150",
@@ -424,46 +449,4 @@ this.MatrixData = [
         }
 
     }
-];   
-}
-onChange(e) {
-    var val = e.target.value;
-    var searchTag= document.getElementById('search');
-    searchTag.innerHTML = val === '' ? '' : '.row:not([data-search*="'+ val +'"]){order:0; opacity: .3;}';
-}
-render() {
-    return <div>
-            <div className="matrix-container">
-                <div className="search-wrap">
-                    <div className="search">
-                        <input type="search" name="search" onChange={this.onChange.bind(this)}/>
-                        <input type="submit" value="Submit"/>
-                    </div>
-                </div>
-            </div>
-            <div className="matrix-gallery">
-                <div className="matrix-gallery-inner">
-                <MatrixRow data={this.MatrixData}/>
-                </div>
-            </div>
-        </div>;
-    }
-}
-
-export default MainComponent;
-
-
-
-
-class MainComponent1 extends React.Component {
-    render() {
-        
-        return (
-            
-            <MainComponent />
-    );
-    }
-
-}
-
-export default MainComponent1;
+];
